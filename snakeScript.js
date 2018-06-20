4eece36c4868c05408a1eca82cfd3ede84a8ea29
Castros2 <castros2@uci.edu>
2018-06-19 23:28:49 -0700

    Snake movement implemented

--- snakeScript.js
+++ snakeScript.js
@@ -4,6 +4,9 @@ testParagraph.textContent = "Below is a work in progress";
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
+var directionX = 5;
+var directionY = 0;
+
 var myCircle={
 	centerX:70,
 	centerY:70,
@@ -11,7 +14,13 @@ var myCircle={
 	fill:'blue'
 }
 
-window.addEventListener('keydown',this.check,false);
+var link={
+	x:70,
+	y:70,
+	width:10,
+	height:10,
+	fill:'black'
+}
 
 function redraw(){
 
@@ -26,30 +35,90 @@ function redraw(){
     ctx.fill();
 }
 
-redraw();
+function redrawRect(){
+	ctx.clearRect(0,0,c.width,c.height);
+
+	ctx.beginPath();
+	ctx.rect(link.x, link.y, link.width, link.height)
+	ctx.closePath();
+	ctx.fillStyle = link.fill;
+	ctx.fill();
+}
+
+function checkBoundaries(obj){
+	// if(obj.x <= 0) {
+	// 	obj.x = 0;
+	// }
+	rightBoundary = c.width;
+	leftBoundary = 0;
+	topBoundary = 0;
+	bottomBoundary = c.height;
+	if (obj.x > rightBoundary){
+		obj.x = leftBoundary;
+	}
+	if (obj.x < leftBoundary){
+		obj.x = rightBoundary;
+	}
+	if(obj.y > bottomBoundary){
+		obj.y = topBoundary;
+	}
+	if(obj.y < topBoundary){
+		obj.y = bottomBoundary;
+	}
+}
 
 function check(e) {
     var code = e.keyCode;
     switch (code) {
         case 37: 
-        	myCircle.centerX -= 10;
-        	redraw();
+        	directionX = -5;
+        	directionY = 0;
+        	// redraw();
         	break; //Left key
         case 38:
-        	myCircle.centerY -= 10;
-        	redraw();
+        	directionX = 0;
+        	directionY = -5;
+        	// redraw();
         	break; //Up key
         case 39:
-        	myCircle.centerX += 10;
-        	redraw();
+        	directionX = 5;
+        	directionY = 0;
+        	// redraw();
         	break; //Right key
         case 40:
-        	myCircle.centerY += 10;
-        	redraw(); 
+        	directionX = 0;
+        	directionY = 5;
+        	// redraw(); 
         	break; //Down key
         // default: alert(code); //Everything else
     }
 }
+
+/*
+* Starting with the semicolon is in case whatever line of code above this example
+* relied on automatic semicolon insertion (ASI). The browser could accidentally
+* think this whole example continues from the previous line. The leading semicolon
+* marks the beginning of our new line if the previous one was not empty or terminated.
+*/
+// redraw();
+redrawRect();
+
+;(function () {
+  function main() {
+    window.requestAnimationFrame( main );
+
+    link.x += directionX;
+    link.y += directionY;
+    checkBoundaries(link);
+    redrawRect();
+    
+    // Your main loop contents.
+    window.addEventListener('keydown',this.check,false);
+
+  }
+  
+  main(); // Start the cycle
+})();
 
 			// var randomNumber = Math.floor(Math.random() * 100) + 1;
 
