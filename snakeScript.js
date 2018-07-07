f0f0ff73513dc519d34811cb821aa01081d2db68
Castros2 <castros2@uci.edu>
2018-07-07 15:34:56 -0700

    snake game now fully playable, beta stage

--- snakeScript.js
+++ snakeScript.js
@@ -22,6 +22,14 @@ var link={
 	fill:'black'
 }
 
+var food={
+	x:0,
+	y:0,
+	width:20,
+	height:20,
+	fill:'red'
+}
+
 function redraw(){
 
     // clear the canvas
@@ -35,8 +43,18 @@ function redraw(){
     ctx.fill();
 }
 
-function redrawRect(){
-	ctx.clearRect(0,0,c.width,c.height);
+function redrawRect(counter){
+	if (typeof redrawRect.linkQueue == 'undefined'){
+		redrawRect.linkQueue = [];
+	}
+
+	if(counter == linkLength){
+		var lastLink =  redrawRect.linkQueue.shift();
+		//console.log(lastLink);
+		ctx.clearRect(lastLink.x,lastLink.y,link.width,link.height);
+	}
+	redrawRect.linkQueue.push({x:link.x, y:link.y});
+	
 
 	ctx.beginPath();
 	ctx.rect(link.x, link.y, link.width, link.height)
@@ -44,6 +62,44 @@ function redrawRect(){
 	ctx.fillStyle = link.fill;
 	ctx.fill();
 }
+
+function redrawFood(){
+	ctx.clearRect(food.x,food.y,food.width,food.height);
+
+	food.x = getRandomInt(10, c.width - 10);
+	food.y = getRandomInt(10, c.height - 10);
+
+	ctx.beginPath();
+	ctx.rect(food.x, food.y, food.width, food.height)
+	ctx.closePath();
+	ctx.fillStyle = food.fill;
+	ctx.fill();
+
+}
+
+function checkFoodCollision(){
+	if (link.x < food.x + food.width &&
+		link.x + link.width > food.x &&
+		link.y < food.y + food.height &&
+		link.y + link.height > food.y){
+		return 1;
+	}
+	return 0;
+}
+
+function checkSnakeCollision(){
+	for(var i = 0; i < redrawRect.linkQueue.length - 1; i++){
+		//console.log(redrawRect.linkQueue[i])
+		if (link.x < redrawRect.linkQueue[i].x + link.width &&
+			link.x + link.width > redrawRect.linkQueue[i].x &&
+			link.y < redrawRect.linkQueue[i].y + link.height &&
+			link.y + link.height > redrawRect.linkQueue[i].y){
+			return 1;
+		}
+	}
+	return 0;
+}
+
 
 function checkBoundaries(obj){
 	// if(obj.x <= 0) {
@@ -70,28 +126,45 @@ function checkBoundaries(obj){
 function check(e) {
     var code = e.keyCode;
     switch (code) {
-        case 37: 
-        	directionX = -5;
-        	directionY = 0;
+        case 37:
+        	//Don't allow to back up into itself
+        	if(directionX !== Math.abs(speed)){ 
+        		directionX = -Math.abs(speed);
+        		directionY = 0;
+        	}
         	// redraw();
         	break; //Left key
         case 38:
-        	directionX = 0;
-        	directionY = -5;
+        	if(directionY !== Math.abs(speed)){
+        		directionX = 0;
+        		directionY = -Math.abs(speed);
+        	}
         	// redraw();
         	break; //Up key
         case 39:
-        	directionX = 5;
-        	directionY = 0;
+        	if(directionX !== -Math.abs(speed)){
+        		directionX = Math.abs(speed);
+        		directionY = 0;
+        	}
         	// redraw();
         	break; //Right key
         case 40:
-        	directionX = 0;
-        	directionY = 5;
+        	if(directionY !== -Math.abs(speed)){
+        		directionX = 0;
+        		directionY = Math.abs(speed);
+        	}
         	// redraw(); 
         	break; //Down key
+        case 73:
+        	linkLength += 5;
+        	console.log('Link Length: ' + linkLength);
+        	break;
         // default: alert(code); //Everything else
     }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /*
@@ -101,24 +174,95 @@ function check(e) {
 * marks the beginning of our new line if the previous one was not empty or terminated.
 */
 // redraw();
-redrawRect();
+var counter = 0;
+redrawRect(counter);
+redrawFood();
 
+var linkLength = 0;
+var speed = 10;
+
+var directionX = Math.abs(speed);
+var directionY = 0;
+
+var myReq;
 ;(function () {
   function main() {
-    window.requestAnimationFrame( main );
 
-    link.x += directionX;
-    link.y += directionY;
-    checkBoundaries(link);
-    redrawRect();
-    
-    // Your main loop contents.
-    window.addEventListener('keydown',this.check,false);
+    // request another frame
+
+    myReq = window.requestAnimationFrame( main );
+
+    // calc elapsed time since last loop
+
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+
+		// Get ready for next frame by setting then=now, but also adjust for your
+	    // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+	    then = now - (elapsed % fpsInterval);
+
+	    // window.requestAnimationFrame( main );
+	    // Your main loop contents.
+	    link.x += directionX;
+	    link.y += directionY;
+	    checkBoundaries(link);
+	  	redrawRect(counter);
+	  	
+
+	  	if(counter < linkLength){
+	  		counter += 1;
+	  	}
+
+	  	if (checkFoodCollision() == 1 ){
+	  		console.log('yummy');
+	  		linkLength += 5;
+	  		redrawFood();
+	  	}
+
+	  	if (checkSnakeCollision() == 1){
+	  		console.log('ouch');
+	  		gameOver();
+	  		// return;
+	  	}
+	    
+	    
+	    
+	    window.addEventListener('keydown',this.check,false);
+	}
 
   }
   
-  main(); // Start the cycle
+var stop = false;
+var frameCount = 0;
+//var $results = $("#results");
+var fps, fpsInterval, startTime, now, then, elapsed;
+
+
+// initialize the timer variables and start the animation
+
+function startAnimating(fps) {
+	fpsInterval = 1000 / fps;
+	then = Date.now();
+	startTime = then;
+	main();
+	// return;
+}
+
+startAnimating(60);
+
+ // main(); // Start the cycle
 })();
+
+function gameOver(){
+	window.cancelAnimationFrame(myReq);
+	ctx.font = "50px Source Sans Pro";
+	ctx.textAlign = "center";
+	ctx.clearRect(0,0,c.width,c.height);
+	ctx.strokeText("Game Over!", c.width/2, c.height/2);
+}
 
 			// var randomNumber = Math.floor(Math.random() * 100) + 1;
 
