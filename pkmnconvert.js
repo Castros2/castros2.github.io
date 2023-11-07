70c853ca6bba6b5c6508e52643376d2b4c4e5599
Stephen Castro <9324941+Castros2@users.noreply.github.com>
2023-11-06 22:08:22 -0800

    Finish implementation of pkmnconvert, add some style

--- pkmnconvert.js
+++ pkmnconvert.js
@@ -6,10 +6,27 @@ function handleConvert() {
 
     const inputArea = document.getElementById("input-area");
     var lines = inputArea.value.split("\n");
-    for (var j = 0; j < lines.length; j++) {
-        var content = document.createTextNode(lines[j] + "\n");
-        output.appendChild(content);
-    }
 
-    fetch("./data.json").then(r=>output.appendChild(r.text()))
+    fetch("./pkmndata.json")
+      .then((response) => response.json())
+      .then((data) => {
+        for (var j = 0; j < lines.length; j++) {
+            var line = lines[j];
+            var lineSplit = line.split(" ");
+            if (lineSplit.length > 2) {
+                var number = lineSplit.pop();
+                var set_ = lineSplit.pop();
+
+                var key = set_.concat(" ", number);
+                var quantity = lineSplit[0];
+                if (data.hasOwnProperty(key)) {
+                    var tcgLine = quantity.concat(" ", data[key]);
+
+                    var content = document.createTextNode(tcgLine + "\n");
+                    output.appendChild(content);
+                }
+            }
+        }
+      })
+      .catch(console.error);
 }
